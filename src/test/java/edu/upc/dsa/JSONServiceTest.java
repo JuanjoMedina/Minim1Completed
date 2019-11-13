6b69795b0a507bd0d061d15b2699d1afabd0304c
Pere <pcf3333@gmail.com>
2019-11-13 23:03:49 +0100

    aa

--- src/test/java/edu/upc/dsa/JSONServiceTest.java
+++ src/test/java/edu/upc/dsa/JSONServiceTest.java
@@ -9,17 +9,22 @@ import static org.junit.Assert.*;
  */
 public class JSONServiceTest {
     @Test
-    public void getTrack() throws Exception {
+    public void getProducts() throws Exception {
 
     }
 
     @Test
-    public void getTrackInJSON() throws Exception {
+    public void newComanda() throws Exception {
 
     }
 
     @Test
-    public void createTrackInJSON() throws Exception {
+    public void manager() throws Exception {
+
+    }
+
+    @Test
+    public void SC() throws Exception {
 
     }
 
